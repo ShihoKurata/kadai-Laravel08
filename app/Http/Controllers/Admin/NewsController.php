e638b9b398fab329e111d4c2b29af6efedeab0be
kurata <shihokurata508@gmail.com>
2021-07-10 05:20:18 +0000

    PHP/Laravel 15 投稿したニュース一覧を表示しよう

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,7 +9,6 @@ use App\News;
 
 class NewsController extends Controller
 {
-    //
     public function add()
   {
       return view('admin.news.create');
@@ -39,5 +38,16 @@ class NewsController extends Controller
         
         return redirect('admin/news/create');
     }
-}
-
+    
+public function index(Request $request)
+{
+    $cond_title = $request->cond_title;
+    if ($cond_title !='') {
+        $posts = News::where('title',$cond_title)->get();
+    } else {
+        $posts = News::all();
+    }
+    return view('admin.news.index', ['posts' => $posts, 'cond_title'=>$cond_title]);
+    }
+    
+ }
